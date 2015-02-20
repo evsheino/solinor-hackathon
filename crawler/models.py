db774ce7f8e70505c61ef665fa6aee91c70c1813
Erkki Heino <evsheino@gmail.com>
2015-02-20 21:12:05 +0200

    REST API for Site model

--- crawler/models.py
+++ crawler/models.py
@@ -6,13 +6,13 @@ from predictor import Predictor
 class Site(models.Model):
     url = models.CharField(max_length=500)
     ip_address = models.CharField(max_length=500)
-    location = models.CharField(max_length=100)
-    company_name = models.CharField(max_length=500)
-    webserver = models.CharField(max_length=50)
-    programming_language = models.CharField(max_length=50)
-    certificate = models.CharField(max_length=500)
-    certificate_authority = models.CharField(max_length=500)
-    html_version = models.CharField(max_length=500)
+    location = models.CharField(max_length=100, blank=True)
+    company_name = models.CharField(max_length=500, blank=True)
+    webserver = models.CharField(max_length=50, blank=True)
+    programming_language = models.CharField(max_length=50, blank=True)
+    certificate = models.CharField(max_length=500, blank=True)
+    certificate_authority = models.CharField(max_length=500, blank=True)
+    html_version = models.CharField(max_length=500, blank=True)
 
     #used for crawling and analyzing the website
     dom = None
